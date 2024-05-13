509d6632cf7aaf67c44a28ac13e559aed7f01496
Thomas Halloran <thallor1@umd.edu>
2024-05-13 17:14:20 -0400

    conf py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+import pyMACS
 project = 'pyMACS'
 copyright = '2024, Thomas Halloran'
 author = 'Thomas Halloran'
